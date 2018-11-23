4875483dc37945004ac4f07255cdd50441c151fc
nicocouz <nicolasscousin@gmail.com>
2018-11-23 17:04:02 +0100

    MAJ

---
+++ twitter.rb
@@ -0,0 +1,20 @@
+require 'dotenv'
+require 'twitter'
+require 'pry'
+require 'twitter-ads'
+
+Dotenv.load
+
+# quelques lignes qui enregistrent les clés d'APIs
+client = Twitter::Streaming::Client.new do |config|
+  config.consumer_key        = ENV["TWITTER_API_KEY"]
+  config.consumer_secret     = ENV["TWITTER_API_SECRET"]
+  config.access_token        = ENV["TWITTER_API_RANDOM"]
+  config.access_token_secret = ENV["TWITTER_API_DOMRAN"]
+end
+
+# Stream mentions of Federer
+topics = ["Federer"]
+client.filter(track: topics.join(",")) do |object|
+  puts object.text if object.is_a?(Twitter::Tweet)
+end
